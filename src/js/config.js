ffdcfda7cefafc259d80ce8e906c7132189a59be
cerberus140 <munyak140@gmail.com>
2021-05-31 08:45:05 -0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'b13a1fd9-2d86-40dd-9c53-118af4edf87c';
+export const MODAL_CLOSE_SEC = 2.5;
